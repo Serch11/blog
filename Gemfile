6990df2b5ade5fe5ca446c2a9e15ab440d32727d
Serch <sredondo95@gmail.com>
2020-09-13 23:04:19 -0500

    mensaje

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+ gem 'record_tag_helper', '~> 1.0'
 #Annote Model
 gem 'annotate'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative '../lib/timer'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +10,7 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.middleware.insert_before(Rack::Sendfile,Timer)
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
